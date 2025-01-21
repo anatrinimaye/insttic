1f33b70dfe3e65ae66adfe66567eb7cc1d3ac323
anaTrini <anatrinimaye@gmail.com>
2025-01-21 22:11:30 +0100

    modificando la carpeta componentes

--- componentes/footer.php
+++ componentes/footer.php
@@ -5,16 +5,16 @@
 
 <div class="container-fluit mt-5 footer">
     <div class="container d-flex py-2 w-50 foot mb-3">
-        <div class="px-3 mt-3 pt-4 w-auto">
-            <a href="" class=""><i class="fa-solid fa-location-dot p-2"></i> <br> INSTTIC  <br> OYALA <br>  Avenida LA PAZ <br> </a>
-        </div>
-        <!-- Mapa de locatizacion -->
-        <div class="mapa w-50">
-
+        <div class="mapa">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d33588.18734957547!2d10.791447858952605!3d1.5844421678333265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10876f87668eae3b%3A0xd22214566ae82f52!2sINSTTIC!5e0!3m2!1ses!2s!4v1737490126338!5m2!1ses!2s" 
+                width="600" height="300" style="border:0;" 
+                allowfullscreen="" loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
         </div>
     </div>
     <!-- Enlaces de los contactos -->
-    <div class="container w-75 contactos">
+    <div class="container-fluit contactos pb-2">
         <div class="mt-3">
             <a href="" class="mt-2"> <i class="fa-solid fa-envelope p-2"></i> <br> insttic@gmail.com </a> 
         </div>
